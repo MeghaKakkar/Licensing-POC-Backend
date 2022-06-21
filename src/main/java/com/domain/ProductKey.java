20bcb600566acdd65acad75565bf7e9e629b563e
amanarora380 <45454931+amanarora380@users.noreply.github.com>
2022-06-21 16:36:31 +0530

    Added ProductKeyController

---
+++ src/main/java/com/domain/ProductKey.java
@@ -0,0 +1,20 @@
+package com.domain;
+
+public class ProductKey {
+	
+	private String licensekey;
+	private String applicationName;
+	public String getLicensekey() {
+		return licensekey;
+	}
+	public void setLicensekey(String licensekey) {
+		this.licensekey = licensekey;
+	}
+	public String getApplicationName() {
+		return applicationName;
+	}
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+
+}
