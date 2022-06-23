03de902584722e8e21f18b63f554c2f19a11b139
suraj <sksuraj1718@gmail.com>
2022-06-23 16:28:14 +0530

    refactored

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
